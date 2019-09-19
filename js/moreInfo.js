c4255eec0f85d995974cc6df7a3409119f12d35d
izajasz13 <karoldd13@gmail.com>
2019-09-19 22:25:59 +0200

    Finished pop up boxes

--- js/moreInfo.js
+++ js/moreInfo.js
@@ -1,18 +1,35 @@
+const box = document.querySelector('box');
 const infos = document.querySelectorAll('.info');
 const adds = document.querySelectorAll('.add');
-const box = document.querySelector('box');
+
+function updateData(e, index){
+    const date = e.target.parentNode.querySelector('.add-date');
+    const info = e.target.parentNode.querySelector('textarea');
+    const box = e.target.parentNode.parentNode;
+    tasks[index].date = date.value;
+    tasks[index].info = info.value;
+    populateList(tasks, list);
+    const infIco = document.querySelectorAll('.info');
+    const adIco = document.querySelectorAll('.add');
+    adIco.forEach(e => e.addEventListener('click', addPopUp));
+    infIco.forEach(e => e.addEventListener('click', infoPopUp));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    document.body.removeChild(box);
+}
 
 function infoPopUp(e){
-    const name = e.target.parentNode.parentNode.querySelector('.name').innerText;
+    const li = e.target.parentNode.parentNode;
+    const name = li.querySelector('.name').innerText;
+    const index = li.querySelector('input').dataset.index;
     const popUpBox = document.createElement('div');
     popUpBox.setAttribute('class', "popUpBox box");
     popUpBox.innerHTML = `
         <div class="cross"></div>
         <h2>More info about ${name}</h2>
         <div class="description">
-            <span class="date-info">21.08.2019</span>
+            <span class="date-info">${tasks[index].date}</span>
             <div class="more-info">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                ${tasks[index].info}
             </div>
         </div>       
     `;
@@ -22,7 +39,9 @@ function infoPopUp(e){
 }
 
 function addPopUp(e){
-    const name = e.target.parentNode.parentNode.querySelector('.name').innerText;
+    const li = e.target.parentNode.parentNode;
+    const name = li.querySelector('.name').innerText;
+    const index = li.querySelector('input').dataset.index;
     const popUpBox = document.createElement('div');
     popUpBox.setAttribute('class', "popUpBox box");
     popUpBox.innerHTML = `
@@ -40,7 +59,9 @@ function addPopUp(e){
     `;
     document.body.insertBefore(popUpBox, box);
     const cross = popUpBox.querySelector('.cross');
-    cross.addEventListener('click', () => document.body.removeChild(popUpBox)); 
+    cross.addEventListener('click', () => document.body.removeChild(popUpBox));
+    const saveButton = popUpBox.querySelector('.save-button');
+    saveButton.addEventListener('click', function(e){updateData(e, index)});
 }
 
 infos.forEach(e => e.addEventListener('click', infoPopUp));
--- js/taskHandler.js
+++ js/taskHandler.js
@@ -8,24 +8,31 @@ function addTask(e){
     const text = this.querySelector('.bar').value;
     const task = {
         text,
-        done: false
+        done: false,
+        date: '',
+        info: ''
     };
     tasks.push(task);
     populateList(tasks, list);
+    const infIco = document.querySelectorAll('.info');
+    const adIco = document.querySelectorAll('.add');
+    adIco.forEach(e => e.addEventListener('click', addPopUp));
+    infIco.forEach(e => e.addEventListener('click', infoPopUp));
     localStorage.setItem('tasks', JSON.stringify(tasks));
     this.reset();
 }
 
 function populateList(items = [], itemsList){
     itemsList.innerHTML = items.map((item, i) => {
+        const icon = (item.date || item.info) ? '<i class="fas fa-info info"></i>' : '<i class="fas fa-plus add"></i>';
         return `
             <li class="task">
                 <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? 'checked' : ''}/>
                 <label for="item${i}"><span class="name">${item.text}</span></label>
-                <span class="icons"><i class="fas fa-info info"></i><i class="fas fa-plus add"></i></span>
+                <span class="icons">${icon}</span>
             </li>
         `;
-    }).join('');
+    }).join('');    
 }
 
 function toggleDone(e){
@@ -35,7 +42,10 @@ function toggleDone(e){
     tasks[index].done = !tasks[index].done;
     localStorage.setItem('tasks', JSON.stringify(tasks));
     populateList(tasks, list);
-    console.log('check');
+    const infIco = document.querySelectorAll('.info');
+    const adIco = document.querySelectorAll('.add');
+    adIco.forEach(e => e.addEventListener('click', addPopUp));
+    infIco.forEach(e => e.addEventListener('click', infoPopUp));
 }
 
 form.addEventListener('submit', addTask);
